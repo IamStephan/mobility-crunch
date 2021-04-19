ad954eda8fdaf046f3963ecca1d2165feed7b6ac
iamstephan <stephanburger54@gmail.com>
2021-04-19 18:53:15 +0200

    added dummy view

---
+++ components/dummy_view.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+const DummyView = () => {
+  return (
+    <View>
+      <Text>Dummy View</Text>
+    </View>
+  )
+}
+
+export default DummyView
