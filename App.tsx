20fd7d6641339fa29aa5585d7c0d0a0bdd3a70b9
iamstephan <stephanburger54@gmail.com>
2021-04-22 15:53:20 +0200

    wrapped provider and protal host for modals

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar"
 import React from "react"
 import { SafeAreaProvider } from "react-native-safe-area-context"
+import { PortalProvider, PortalHost } from "@gorhom/portal"
 
 import useCachedResources from "./hooks/useCachedResources"
 import useColorScheme from "./hooks/useColorScheme"
 import Navigation from "./navigation"
+import { PortalHosts } from "./constants/portal_hosts"
 
 export default function App() {
   const isLoadingComplete = useCachedResources()
@@ -15,7 +17,11 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+        <PortalProvider>
+          <Navigation colorScheme={colorScheme} />
+          <PortalHost name={PortalHosts.modals} />
+        </PortalProvider>
+
         <StatusBar />
       </SafeAreaProvider>
     )
