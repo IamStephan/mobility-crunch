90ff6c3f5c600ff0cf4e55f642b2aa7960b2344d
iamstephan <stephanburger54@gmail.com>
2021-04-24 20:34:21 +0200

    Settings hook for data and data mutations

---
+++ database_hooks/settings/useSettings.ts
@@ -0,0 +1,53 @@
+import { useState } from "react"
+import { useDocumentData } from "react-firebase-hooks/firestore"
+
+import { DatabaseReturnType } from "../types"
+import { useFirebase, State } from "../../stores/useFirebase"
+
+const selector = (state: State) => ({ firebase: state.firebase })
+
+export interface SettingsMutations {
+  setVatValue: (vatValue: number) => Promise<void>
+}
+
+export interface SettingsData {
+  current_forma_num: number
+  current_invoice_num: number
+  current_quote_num: number
+  debug: boolean
+  vat: number
+}
+
+export const useSettingsData = (): DatabaseReturnType<SettingsData> => {
+  const { firebase } = useFirebase(selector)
+
+  const [data, loading, error] = useDocumentData<SettingsData>(
+    firebase.firestore().collection("settings").doc("v1")
+  )
+
+  return {
+    state: {
+      loading,
+      error,
+    },
+    data,
+  }
+}
+
+export const useSettingsMutations = (): SettingsMutations => {
+  const { firebase } = useFirebase(selector)
+
+  const setVatValue = async (vatValue: number) => {
+    await firebase
+      .firestore()
+      .collection("settings")
+      .doc("v1")
+      .update({
+        vat: vatValue,
+      } as Pick<SettingsData, "vat">)
+  }
+
+  return {
+    setVatValue,
+  }
+}
