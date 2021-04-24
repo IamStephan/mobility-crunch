98c37fdc201860156a577f493ef4cd4c7f02cee3
iamstephan <stephanburger54@gmail.com>
2021-04-24 20:34:33 +0200

    exporting settings hook

---
+++ database_hooks/index.ts
@@ -0,0 +1,10 @@
+// Types
+export { DatabaseReturnType } from "./types"
+
+// Database
+export {
+  useSettingsData,
+  useSettingsMutations,
+  SettingsData,
+  SettingsMutations,
+} from "./settings/useSettings"
