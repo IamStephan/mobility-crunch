a53b53a9306aeb84a97c40853473ce1a99a98c99
iamstephan <stephanburger54@gmail.com>
2021-04-24 20:30:49 +0200

    ignore Firebase warnings for react native expo

--- App.tsx
+++ App.tsx
@@ -2,11 +2,24 @@ import { StatusBar } from "expo-status-bar"
 import React from "react"
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import { PortalProvider, PortalHost } from "@gorhom/portal"
+import { LogBox } from "react-native"
 
 import useCachedResources from "./hooks/useCachedResources"
 import useColorScheme from "./hooks/useColorScheme"
 import Navigation from "./navigation"
 import { PortalHosts } from "./constants/portal_hosts"
+
+/**
+ * Ignore warning produced by the firebase SDK
+ *
+ * This error occurs because I'm using the JS version
+ * of the SDK and not the native one. This SDK uses
+ * setTimeout/setInterval and React Native discourages
+ * the use of these methods with long periods of time.
+ *
+ * Expo...
+ */
+LogBox.ignoreLogs(["Setting a timer for a long period of time"])
 
 export default function App() {
   const isLoadingComplete = useCachedResources()
