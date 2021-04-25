10cfb5453ed2191b724589c08fda1dc96d7fada9
iamstephan <stephanburger54@gmail.com>
2021-04-25 14:39:57 +0200

    more readable and consistent

--- navigation/root/index.tsx
+++ navigation/root/index.tsx
@@ -1,61 +1,71 @@
 import React from "react"
+import { StyleSheet } from "react-native"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
-
 //@ts-ignore
 import Icon from "@expo/vector-icons/MaterialIcons"
 
-// Constants
 import { NavContainers } from "../../constants/screens"
-
-// Container
 import InventoryNavigator from "../inventory"
 import OrdersNavigator from "../orders"
 import SettingsNavigator from "../settings"
 
 const RootTabNavigator = createBottomTabNavigator()
 
+interface TabOptionsI {
+  tabBarLabel: string
+  tabBarIcon: (props: {
+    focused: boolean
+    color: string
+    size: number
+  }) => React.ReactNode
+}
+
+const TabOptions = (label: string, icon: string): TabOptionsI => {
+  const TabIcon: TabOptionsI["tabBarIcon"] = ({ color, size }) => (
+    <Icon name={icon} color={color} size={size} />
+  )
+
+  return {
+    tabBarLabel: label,
+    tabBarIcon: TabIcon,
+  }
+}
+
+const InventoryTabOptions = TabOptions("Inventory", "inventory")
+const OrdersTabOptions = TabOptions("Orders", "assignment")
+const SettingsTabOptions = TabOptions("Settings", "settings")
+
 const RootNavigator = () => {
   return (
     <RootTabNavigator.Navigator
       initialRouteName={NavContainers.orderItemContainer}
       tabBarOptions={{
-        style: {
-          elevation: 0,
-        },
+        style: styles.header,
       }}
     >
       <RootTabNavigator.Screen
         name={NavContainers.inventoryItemContainer}
-        options={{
-          tabBarLabel: "Inventory",
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="inventory" color={color} size={size} />
-          ),
-        }}
+        options={InventoryTabOptions}
         component={InventoryNavigator}
       />
       <RootTabNavigator.Screen
         name={NavContainers.orderItemContainer}
-        options={{
-          tabBarLabel: "Orders",
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="assignment" color={color} size={size} />
-          ),
-        }}
+        options={OrdersTabOptions}
         component={OrdersNavigator}
       />
       <RootTabNavigator.Screen
         name={NavContainers.settingsContainer}
-        options={{
-          tabBarLabel: "Settings",
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="settings" color={color} size={size} />
-          ),
-        }}
+        options={SettingsTabOptions}
         component={SettingsNavigator}
       />
     </RootTabNavigator.Navigator>
   )
 }
+
+const styles = StyleSheet.create({
+  header: {
+    elevation: 0,
+  },
+})
 
 export default RootNavigator
