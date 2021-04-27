6d6f392f40dde73fd6f640d3052a1ef057fc16d3
iamstephan <stephanburger54@gmail.com>
2021-04-27 02:52:55 +0200

    exporting product fetcher hook

--- database_hooks/index.ts
+++ database_hooks/index.ts
@@ -1,7 +1,27 @@
+/**
+ * NOTE:
+ * =====
+ * Collection methods need to be optimized for reads
+ * currently they get the entire collection instead of just paginating
+ * the collections. The reasoning behind this is to allow for client
+ * side text filtering and search as firestore does not support this,
+ * and using a third party service would incur more costs
+ *
+ * TODO: find a way to store and update data locally (Adapter pattern?)
+ */
+
 // Types
 export { DatabaseReturnType } from "./types"
 
-// Database
+/**
+ * PRODUCTS
+ */
+export { useProductsData } from "./products/useProducts"
+export { ProductsData } from "./products/types"
+
+/**
+ * SETTINGS
+ */
 export {
   useSettingsData,
   useSettingsMutations,
