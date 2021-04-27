21c75738a3ca5a65aaf86bd46b35798bf4b039de
iamstephan <stephanburger54@gmail.com>
2021-04-27 02:54:01 +0200

    search seaction for products

---
+++ sections/inventory_item_list_search/index.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import { View, StyleSheet } from "react-native"
+
+import Searchbox from "../../components/search"
+import Layout from "../../constants/layout"
+
+interface Props {
+  onSearch: (text: string) => void
+}
+
+const InventoryItemListSearchSection: React.FC<Props> = ({ onSearch }) => {
+  return (
+    <View style={styles.container}>
+      <Searchbox onSearch={onSearch} iconName="plus-circle-outline" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: Layout.spacing * 2,
+  },
+})
+
+export default InventoryItemListSearchSection
