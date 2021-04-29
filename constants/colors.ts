99b63e15473ee8a29a00a3d9cea6d77928e21d0e
iamstephan <stephanburger54@gmail.com>
2021-04-29 20:57:24 +0200

    new constants

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,19 +1,23 @@
-const tintColorLight = "green"
-const tintColorDark = "green"
+export const Gray = {
+  gray100: "#f7fafc",
+  gray200: "#edf2f7",
+  gray300: "#e2e8f0",
+  gray400: "#cbd5e0",
+  gray500: "#a0aec0",
+  gray600: "#718096",
+  gray700: "#4a5568",
+  gray800: "#2d3748",
+  gray900: "#1a202c",
+}
 
-export default {
-  light: {
-    text: "#000",
-    background: "#fff",
-    tint: tintColorLight,
-    tabIconDefault: "#ccc",
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: "#fff",
-    background: "#000",
-    tint: tintColorDark,
-    tabIconDefault: "#ccc",
-    tabIconSelected: tintColorDark,
-  },
+export const Green = {
+  green100: "#f0fff4",
+  green200: "#c6f6d5",
+  green300: "#9ae6b4",
+  green400: "#68d391",
+  green500: "#48bb78",
+  green600: "#38a169",
+  green700: "#2f855a",
+  green800: "#276749",
+  green900: "#22543d",
 }
---
+++ constants/typography.ts
@@ -0,0 +1,12 @@
+export const Typography = {
+  xs: 11,
+  sm: 12,
+  md: 13,
+  lg: 15,
+  xl: 17,
+  "2xl": 19,
+  "3xl": 21,
+  "4xl": 24,
+  "5xl": 27,
+  "6xl": 32,
+}
