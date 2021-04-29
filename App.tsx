55bd855d6a73d328e8622f881d78e2473518a011
iamstephan <stephanburger54@gmail.com>
2021-04-29 17:31:23 +0200

    provider and custom font

--- App.tsx
+++ App.tsx
@@ -1,8 +1,19 @@
 import React from "react"
 import { LogBox } from "react-native"
 import { StatusBar } from "expo-status-bar"
+import AppLoading from "expo-app-loading"
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import { PortalProvider, PortalHost } from "@gorhom/portal"
+import { ThemeProvider, ThemeProviderProps } from "react-native-magnus"
+import {
+  useFonts,
+  Roboto_100Thin,
+  Roboto_300Light,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+  Roboto_900Black,
+} from "@expo-google-fonts/roboto"
 
 import useCachedResources from "./hooks/useCachedResources"
 import useColorScheme from "./hooks/useColorScheme"
@@ -21,19 +32,42 @@ import { PortalHosts } from "./constants/portal_hosts"
  */
 LogBox.ignoreLogs(["Setting a timer for a long period of time"])
 
+const theme: ThemeProviderProps["theme"] = {
+  fontFamily: {
+    normal: "Roboto_400Regular",
+    bold: "Roboto_700Bold",
+    100: "Roboto_100Thin",
+    300: "Roboto_300Light",
+    400: "Roboto_400Regular",
+    500: "Roboto_500Medium",
+    700: "Roboto_700Bold",
+    900: "Roboto_900Black",
+  },
+}
+
 export default function App() {
   const isLoadingComplete = useCachedResources()
   const colorScheme = useColorScheme()
+  const [fontsLoaded] = useFonts({
+    Roboto_100Thin,
+    Roboto_300Light,
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+    Roboto_900Black,
+  })
 
-  if (!isLoadingComplete) {
-    return null
+  if (!isLoadingComplete && !fontsLoaded) {
+    return <AppLoading />
   } else {
     return (
       <SafeAreaProvider>
-        <PortalProvider>
-          <Navigation colorScheme={colorScheme} />
-          <PortalHost name={PortalHosts.modals} />
-        </PortalProvider>
+        <ThemeProvider theme={theme}>
+          <PortalProvider>
+            <Navigation colorScheme={colorScheme} />
+            <PortalHost name={PortalHosts.modals} />
+          </PortalProvider>
+        </ThemeProvider>
 
         <StatusBar />
       </SafeAreaProvider>
