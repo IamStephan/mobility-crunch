4a27a0e859c1b3877d4b32dcdb93883a5d4abe8a
iamstephan <stephanburger54@gmail.com>
2021-05-02 18:33:50 +0200

    rather use the snapshot

--- database_hooks/settings/useSettings.ts
+++ database_hooks/settings/useSettings.ts
@@ -1,4 +1,6 @@
-import { useDocumentData } from "react-firebase-hooks/firestore"
+import { useState, useEffect } from "react"
+
+import { useDocument } from "react-firebase-hooks/firestore"
 
 import { DatabaseReturnType } from "../types"
 import { useFirebase, State } from "../../stores/useFirebase"
@@ -18,16 +20,31 @@ export interface SettingsData {
 }
 
 export const useSettingsData = (): DatabaseReturnType<SettingsData> => {
+  const [data, setData] = useState<SettingsData>()
+
   const { firebase } = useFirebase(selector)
   const query = firebase.firestore().collection("settings").doc("v1")
-  const [data, loading, error] = useDocumentData<SettingsData>(query)
+  const [doc, loading, error] = useDocument<SettingsData>(query)
+
+  useEffect(() => {
+    if (doc) {
+      const data: any = {
+        ...doc.data(),
+        id: doc.id,
+      }
+
+      setData(data)
+    }
+  }, [doc])
+
+  const dataToReturn = data
 
   return {
     state: {
       loading,
       error,
     },
-    data,
+    data: dataToReturn,
   }
 }
 
