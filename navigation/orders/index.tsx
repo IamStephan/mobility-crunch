273fa61607bb43e5bfb9c3a19de944690ca3ff04
iamstephan <stephanburger54@gmail.com>
2021-05-03 23:17:35 +0200

    ability to edit order inside their view

--- navigation/orders/index.tsx
+++ navigation/orders/index.tsx
@@ -1,14 +1,20 @@
-import React from "react"
-import { StyleSheet } from "react-native"
+import React, { useRef, useCallback } from "react"
+import { StyleSheet, View, TouchableOpacity } from "react-native"
 import {
   createStackNavigator,
   StackNavigationOptions,
   HeaderTitle,
   StackHeaderTitleProps,
 } from "@react-navigation/stack"
-import { useRoute } from "@react-navigation/native"
+import { useRoute, useNavigation } from "@react-navigation/native"
 
 import { NavScreens } from "../../constants/screens"
+import OrderItemMoreModal, {
+  RefFunctions as ModalRef,
+} from "../../modal_components/order_item_list_more"
+import Icon from "../../components/icon"
+import { OrdersData } from "../../database_hooks"
+
 import OrderItemListScreen from "../../screens/order_item_list"
 import OrderItemViewScreen from "../../screens/order_item_view"
 import OrderItemAddProductListScreen from "../../screens/order_item_add_product_list"
@@ -31,6 +37,38 @@ const ItemUpsertHeaderTitle: React.FC<StackHeaderTitleProps> = ({
   return <HeaderTitle style={{ color: tintColor }}>New Order</HeaderTitle>
 }
 
+const ItemViewHeaderRight: React.FC<{
+  tintColor?: string | undefined
+}> = ({ tintColor }) => {
+  const { params } = useRoute()
+  const { navigate, goBack } = useNavigation()
+  const modalRef = useRef<ModalRef>(null)
+
+  const _handleOpenModal = useCallback(() => {
+    if (modalRef.current?.openModal) {
+      modalRef.current.openModal(params as OrdersData)
+    }
+  }, [])
+
+  if (!params) return null
+
+  return (
+    <>
+      <TouchableOpacity onPress={_handleOpenModal}>
+        <View style={styles.headerIcon}>
+          <Icon color={tintColor} name="more-vert" size={23} />
+        </View>
+      </TouchableOpacity>
+
+      <OrderItemMoreModal
+        ref={modalRef}
+        navigateTo={navigate}
+        goBack={goBack}
+      />
+    </>
+  )
+}
+
 const ItemListOptions: StackNavigationOptions = {
   headerTitle: "Order",
 }
@@ -41,6 +79,7 @@ const OrderItemUpsertOptions: StackNavigationOptions = {
 
 const OrderItemViewOptions: StackNavigationOptions = {
   headerTitle: "Order overview",
+  headerRight: ItemViewHeaderRight,
 }
 
 const OrderItemAddProductListOptions: StackNavigationOptions = {
