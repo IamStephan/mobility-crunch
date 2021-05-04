539e656d40b4ca212b6401f16271b16f00b02a31
iamstephan <stephanburger54@gmail.com>
2021-05-04 17:50:54 +0200

    alert provider

--- App.tsx
+++ App.tsx
@@ -14,6 +14,7 @@ import {
   Roboto_700Bold,
   Roboto_900Black,
 } from "@expo-google-fonts/roboto"
+import FlashMessage from "react-native-flash-message"
 
 import useCachedResources from "./hooks/useCachedResources"
 import useColorScheme from "./hooks/useColorScheme"
@@ -70,6 +71,7 @@ export default function App() {
         </ThemeProvider>
 
         <StatusBar />
+        <FlashMessage position="top" />
       </SafeAreaProvider>
     )
   }
